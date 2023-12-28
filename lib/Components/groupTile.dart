8a16447717d135ea0c4086cfcfd636c94685efc3
coderbhavya24 <bhavya2015340@akgec.ac.in>
2023-12-28 15:55:56 +0530

    colors structure

---
+++ lib/Components/groupTile.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:splitwise/colors.dart';
+class GroupTile extends StatelessWidget {
+  const GroupTile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/colors.dart
@@ -0,0 +1,16 @@
+import 'dart:ui';
+
+const Color primaryDark = Color(0xFF4ec8bb);
+const Color primaryLight = Color(0xFF6CDAFF);
+
+const Color secondaryDark = Color(0xFF507DFF);
+const Color secondaryLight = Color(0xFF60A6FF);
+
+const Color fontDark = Color(0xFF5C6E8D);
+const Color fontLight = Color(0xFFB3C7E0);
+
+const Color fontHeading = Color(0xFF3a4d62);
+const Color fontSubHeading = Color(0xFFb5b5b5);
+
+const Color accent = Color(0xFFee6a6c);
+const Color background = Color(0xFFF1F2F5);
